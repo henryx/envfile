39d241bcefb4b955c37544130e5333526fb7c340
Enrico Bianchi <enrico.bianchi@gmail.com>
2020-04-11 23:42:38 +0200

    Loaded .env file

--- src/main/java/com/application/envfile/EnvFile.java
+++ src/main/java/com/application/envfile/EnvFile.java
@@ -1,11 +1,18 @@
 package com.application.envfile;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public final class EnvFile {
 
     /**
      * Load .env file as system variable
      */
     public static void load() {
-
+        try (FileReader fs = new FileReader(".env"); BufferedReader br = new BufferedReader(fs)) {
+        } catch (IOException e) {
+            // .env file not exists, nothing happened
+        }
     }
 }
